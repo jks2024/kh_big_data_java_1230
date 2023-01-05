e4336425d9d4ef84f35e498c06ce5d946c3f6acd
jks2024 <jks2024@gmail.com>
2023-01-05 12:46:41 +0900

    반복문연습

---
+++ Java_Public_Data_1230/src/반복문연습문제2번/StarPrintEx.java
@@ -0,0 +1,22 @@
+package 반복문연습문제2번;
+import java.util.Scanner;
+
+// 입력 : 5
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+public class StarPrintEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("별 갯수 입력 : ");
+        int star = sc.nextInt();
+        for(int i = 0; i < star; i++) {
+            for(int j = 0; j < i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
