2fce3d8a6886eefdd75bd5e7011cbc0c15082806
jks2024 <jks2024@gmail.com>
2023-01-09 15:15:02 +0900

    1월9일 배열과 문자열 수업 예제

--- Java_Public_Data_1230/src/상근날드/SetMenu.java
+++ Java_Public_Data_1230/src/상근날드/SetMenu.java
@@ -1,4 +1,7 @@
 package 상근날드;
+
+import java.util.Scanner;
+
 // 상근날드 가게에서는 세트 메뉴를 판매 합니다.
 // 햄거버는 3개 종류가 있고
 // 음료 2가지 종류가 있음
@@ -7,6 +10,21 @@ package 상근날드;
 // 세트 메뉴의 총 금액 구하는 문제
 public class SetMenu {
     public static void main(String[] args) {
+        int[] menu = new int[5]; // 5개의 정수로 구성된 배열 생성
+        Scanner sc = new Scanner(System.in);
+        System.out.print("메뉴 입력 : "); // 화면 출력
+        for(int i = 0; i < menu.length; i++) {
+            menu[i] = sc.nextInt(); // 배열의 길이 만큼 순회하면서 값을 연속으로 입력 받음
+        }
+        int minBurger = menu[0]; // 0 ~ 2 인덱스 사이의 햄버거 중 하나의 값을 기본값을 설정
+        int minDrink = menu[3];  // 3 ~ 4 인덱스 사이의 음료 중 하나의 값을 기본값으로 설정
+        // 반복문을 순회하면서 햄버거1개와 음료1개 선택(제일 싼 것)
+        for(int i = 0; i < menu.length; i++) {
+            if((i < 3) && (minBurger > menu[i])) minBurger = menu[i];
+            if((i > 2) && (minDrink > menu[i])) minDrink = menu[i];
+        }
+        System.out.println(minBurger + minDrink - 50);
+
 
     }
 }
