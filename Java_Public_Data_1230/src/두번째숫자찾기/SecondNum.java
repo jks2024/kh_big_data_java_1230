bc4b3c89e446439d6370e81f675032aa021d46bd
jks2024 <jks2024@gmail.com>
2023-01-19 15:16:05 +0900

    1월19일 오후 디폴트메소드까지

--- Java_Public_Data_1230/src/두번째숫자찾기/SecondNum.java
+++ Java_Public_Data_1230/src/두번째숫자찾기/SecondNum.java
@@ -5,9 +5,28 @@ package 두번째숫자찾기;
 // 찾을 수 : 5
 // 결과 : 6
 // 만약에 2번째 등장하는 수가 없으면 -1 출력
+import java.util.Scanner;
 
 public class SecondNum {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int cnt = 0; // 몇번째 나타나는 수인지 확인
+        System.out.print("임의의 수(10개) : ");
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt(); // 입력 받은 각각의 임의의 수를 배열에 대입
+        }
+        System.out.print("찾을 수 입력 : ");
+        int secondNum = sc.nextInt();
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == secondNum) {
+                cnt++;
+                if(cnt == 2) {
+                    System.out.print(i+1);
+                    break;
+                }
+            }
+        }
+        if(cnt < 2) System.out.println("-1");
     }
 }
