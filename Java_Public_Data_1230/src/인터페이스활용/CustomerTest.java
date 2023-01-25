38cd2cc509593b7cca496578f0db08cb9bf7d933
jks2024 <jks2024@gmail.com>
2023-01-25 15:13:42 +0900

    1월25일 객체지향 끝

---
+++ Java_Public_Data_1230/src/인터페이스활용/CustomerTest.java
@@ -0,0 +1,15 @@
+package 인터페이스활용;
+
+public class CustomerTest {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        Buy buyer = customer;
+        buyer.buy();
+        buyer.order(); // 재정의된 메소드가 호출 됨
+
+        Sell seller = customer;
+        seller.sell();
+        seller.order();
+
+    }
+}
