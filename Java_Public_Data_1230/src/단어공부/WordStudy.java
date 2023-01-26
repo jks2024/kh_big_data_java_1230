42d0952ac4807a3b793e49097b9df873761463a5
jks2024 <jks2024@gmail.com>
2023-01-26 15:15:29 +0900

    1월26일 Calendar 까지 반영

---
+++ Java_Public_Data_1230/src/단어공부/WordStudy.java
@@ -0,0 +1,11 @@
+package 단어공부;
+// 알파벳 대소문자로 이루어진 단어가 주어지면 가장 많이 등장하는 알파벳을 찾는 문제
+// 입력은 대소문자로 입력하고, 결과는 대문자로 출력
+// 만약 가장 많이 등장하는 알파벳이 여러개인 경우는 ? 로 출력
+// 결과 예시 : AaAbcdd => A
+// 결과 예시 : ddcfgrG => ?
+public class WordStudy {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java_Public_Data_1230/src/열거타입예제/DevType.java
@@ -0,0 +1,7 @@
+package 열거타입예제;
+
+public enum DevType {
+    MOBILE, FRONTEND, BACKEND, DBA, EMBEDDED
+}
+
+
